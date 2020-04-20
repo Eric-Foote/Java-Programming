9015e7c2855fd020d08bdf64e30733d510e696ab
clocknight <EricFoote2014@hotmail.com>
2020-04-20 14:47:38 -0300

    Tested to see if the codium extension would work for IDE pratice

---
+++ PraticeProblems/HelloWorld/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+        System.out.println("Testing to make sure that codium is set up properly");
+    }
+}
