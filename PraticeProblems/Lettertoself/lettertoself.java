f076f369b02191ef9eb52a4a97bdc728b654db09
clocknight <EricFoote2014@hotmail.com>
2020-04-20 15:03:43 -0300

    Did the first real pratice problem to pratice tabbing in println and how to format println

---
+++ PraticeProblems/Lettertoself/lettertoself.java
@@ -0,0 +1,21 @@
+/*
+* Write a program that displays your name and address on the screen as if it were a letter.
+*/
+
+public class lettertoself {
+    public static void main(String args[]){
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|\t\t\t\t\t\t\t\t\t###\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t###\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t###\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\tName\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\tAddress\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\tCity,PostalCode\t\t|");
+        System.out.println("|\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("+-------------------------------------------------------------------------------+");        
+
+    }
+}
