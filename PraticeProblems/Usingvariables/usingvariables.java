62630edb174f051cacd41d61a36f10093e16d828
clocknight <EricFoote2014@hotmail.com>
2020-04-20 15:48:59 -0300

    Forgot to add the problems description to the last 2 files so quickly updated them

--- PraticeProblems/Usingvariables/usingvariables.java
+++ PraticeProblems/Usingvariables/usingvariables.java
@@ -1,4 +1,10 @@
-
+/*
+* Write a program that creates three variables: an int, a double, and a String.
+*
+* Put the value 113 into the first variable, the value 2.71828 into the second, and the value "Computer Science" into the third. It does not matter what you call the variables... this time.
+* 
+* Then, display the values of these three variables on the screen, one per line.
+*/
 public class usingvariables {
     public static void main(String[] args) {
          int roomNumber;
