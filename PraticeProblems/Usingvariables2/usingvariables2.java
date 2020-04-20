5d7f82074ed950e145a9fc421e1ad67e740fefdc
clocknight <EricFoote2014@hotmail.com>
2020-04-20 15:41:06 -0300

    Did the second using variables pratice problem

---
+++ PraticeProblems/Usingvariables2/usingvariables2.java
@@ -0,0 +1,10 @@
+
+public class usingvariables2 {
+    public static void main(String[] args) {
+        String name;
+        int year;
+        name = "Eric";
+        year = 2021;
+        System.out.println("My name is " + name + " and I'll graduate in " + year);
+    }
+}
