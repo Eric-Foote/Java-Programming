fa6c40808754dcdddc8e6735bdd77463b3fb55d8
clocknight <EricFoote2014@hotmail.com>
2020-04-20 15:36:56 -0300

    did the pratice problem on using variables

---
+++ PraticeProblems/Usingvariables/usingvariables.java
@@ -0,0 +1,15 @@
+
+public class usingvariables {
+    public static void main(String[] args) {
+         int roomNumber;
+         double e;
+         String goal;
+         roomNumber = 123;
+         e = 2.718;
+         goal = "Computer Science";
+         System.out.println("This is room # " + roomNumber);
+         System.out.println("e is close to " + e);
+         System.out.println("I am learning a bit about " + goal);   
+    }
+
+}
