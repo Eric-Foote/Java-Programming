664cfd05ce54dbcacd58bda3e0b044a33466e34f
clocknight <EricFoote2014@hotmail.com>
2020-04-20 21:33:17 -0300

    Forgot to commit the beginning of the example earlier. I still need to format the output properly to make a pseudo table

---
+++ PraticeProblems/Schedule/schedule.java
@@ -0,0 +1,29 @@
+/*
+* Use several variables to store the names of your classes and their teachers. 
+* Then, display a nice little table displaying your schedule
+*/
+public class schedule {
+    public static void main(String[] args) {
+        String class1 = "Operating Systems";
+        String class2 = "Networking";
+        String class3 = "ECE";
+        String class4 = "ECE Lab";
+        String class5 = "Introduction to Micro Economics";
+        String class6 = "Computer Architecture";
+        String class7 = "Network Programming";
+        String class8 = "Techincal Report";
+        String class9 = "Introduction to Macro Economics";
+        String level1 = "3000";
+        String level2 = "3000";
+        String level3 = "2000";
+        String level4 = "2000";
+        String level5 = "1000";
+        String level6 = "3000";
+        String level7 = "4000";
+        String level8 = "4000";
+        String level9 = "1000";
+        System.out.println("+---------------------------------------------------------------+");
+        
+        System.out.println("+---------------------------------------------------------------+");
+    }
+}   
