67010c1e4cc68c1413fa32f1f18cc37a9acdba01
clocknight <EricFoote2014@hotmail.com>
2020-04-23 18:15:59 -0300

    Put in a comment the original solution to the problem

--- PraticeProblems/NumberGuessingGame/aNumberGuessingGame.java
+++ PraticeProblems/NumberGuessingGame/aNumberGuessingGame.java
@@ -17,3 +17,29 @@ public class aNumberGuessingGame {
         System.out.println("You are correct your guess was what I was thinking of " + x);
     }
 }
+
+// Below is the solution to the original problem 
+/*
+import java.util.Random;
+import java.util.Scanner;
+public class aNumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random r = new Random();
+        int guess;
+        int x = 1 + r.nextInt(10);
+        System.out.println("I am thinking of a number from 1 to 10");
+        System.out.println("Your guess");
+        guess = scan.nextInt();
+        if (guess == x)
+        {
+            System.out.println("You are correct your guess was what I was thinking of " + x);
+        }
+        else
+        {
+            System.out.println("Sorry, but I was thinking of " + x);
+        }
+
+    }
+}
+*/
