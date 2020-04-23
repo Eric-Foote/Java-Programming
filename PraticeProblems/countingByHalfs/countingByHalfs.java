d41803ee89ee350f69fe17b5da50fa05ea3c2ba4
clocknight <EricFoote2014@hotmail.com>
2020-04-23 15:34:29 -0300

    Did the third for loop pratice problem

---
+++ PraticeProblems/countingByHalfs/countingByHalfs.java
@@ -0,0 +1,11 @@
+
+public class countingByHalfs {
+    public static void main(String[] args) {
+        System.out.println("x");
+        System.out.println("-----");
+        for(double x = -10; x <= 10; x= x+ 0.5)
+        {
+            System.out.println(x);
+        }
+    }
+}
