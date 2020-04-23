24c12a95fabb248831e465b4f2fd109752261629
clocknight <EricFoote2014@hotmail.com>
2020-04-23 18:25:53 -0300

    Made additonal modifications to this one to include a counter of the amount of attempts

--- PraticeProblems/NumberGuessingGame/aNumberGuessingGame.java
+++ PraticeProblems/NumberGuessingGame/aNumberGuessingGame.java
@@ -4,6 +4,7 @@ public class aNumberGuessingGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random r = new Random();
+        int attemptCount = 1;
         int guess;
         int x = 1 + r.nextInt(10);
         System.out.println("I am thinking of a number from 1 to 10");
@@ -13,8 +14,10 @@ public class aNumberGuessingGame {
             System.out.println("That is incorrect. Guess again");
             System.out.println("Your guess");
             guess = scan.nextInt();
+            attemptCount++;
         }
         System.out.println("You are correct your guess was what I was thinking of " + x);
+        System.out.println("It only took you " + attemptCount + " tries");
     }
 }
 
