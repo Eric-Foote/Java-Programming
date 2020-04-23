4f5506bce30a0228b828b2baffeebc7aed2891a0
clocknight <EricFoote2014@hotmail.com>
2020-04-23 15:03:16 -0300

    Started thefirst assignment on for loops

---
+++ PraticeProblems/tenTimes/tenTimes.java
@@ -0,0 +1,9 @@
+public class tenTimes {
+    public static void main(String[] args) {
+        String statement = "Testing";
+        for(int i = 1; i< 11; i++)
+        {
+            System.out.println(i + "." + statement);
+        }
+    }
+}
