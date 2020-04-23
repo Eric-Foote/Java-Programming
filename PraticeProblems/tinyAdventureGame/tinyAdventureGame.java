785ff0859224bb8d99de59f67f58f8f1be450d72
clocknight <EricFoote2014@hotmail.com>
2020-04-23 12:04:11 -0300

    Finished the upstairs branch of the decision tree just need tofill out the 4 endings for the kitchen branch

--- PraticeProblems/tinyAdventureGame/tinyAdventureGame.java
+++ PraticeProblems/tinyAdventureGame/tinyAdventureGame.java
@@ -10,14 +10,41 @@ public class tinyAdventureGame {
         firstChoice = scan.next();
         if(firstChoice.equals("upstairs"))
         {
-            System.out.println("Upstairs you see a hallway. At the end of the hallway is the master \"bedroom.\". There is also a \"bathroom\" off the hallway. Where would you like to go?")
-            secondChoice = scan.Next();
+            System.out.println("Upstairs you see a hallway. At the end of the hallway is the master \"bedroom.\". There is also a \"bathroom\" off the hallway. Where would you like to go?");
+            secondChoice = scan.next();
             if(secondChoice.equals("bedroom"))
             {
-
+                System.out.println("You are in a plush bedroom, with expensive-looking hardwood furniture. The bed is unmade. In the back of the room, the closet door is ajar. Would you like to open the door? (\"yes\" or \"no\")");
+                thirdChoice = scan.next();
+                if(thirdChoice.equals("yes"))
+                {
+                    System.out.println("YOU WIN... nothing");
+                }
+                else if(thirdChoice.equals("no"))
+                {
+                    System.out.println("Oh well... you leave");
+                }
+                else 
+                {
+                    System.out.println("Oh well you leave");
+                }
             }
             else if(secondChoice.equals("bathroom"))
             {
+                System.out.println("You are in a bathroom, with all the bathroom fixtures, the closet door is ajar. Would you like to open the door? (\"yes\" or \"no\")");
+                thirdChoice = scan.next();
+                if(thirdChoice.equals("yes"))
+                {
+                    System.out.println("YOU WIN... nothing");
+                }
+                else if(thirdChoice.equals("no"))
+                {
+                    System.out.println("Oh well... you leave");
+                }
+                else 
+                {
+                    System.out.println("Oh well you leave");
+                }
 
             }
             else 
