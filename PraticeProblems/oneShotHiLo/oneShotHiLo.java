e28ddf018c85f0b55f306bd0fe728ef335a442d9
clocknight <EricFoote2014@hotmail.com>
2020-04-23 22:07:33 -0300

    Did the modification to this program to give it 7 attempts so its not quite oneShot any more but I just reused some code

--- PraticeProblems/oneShotHiLo/oneShotHiLo.java
+++ PraticeProblems/oneShotHiLo/oneShotHiLo.java
@@ -6,6 +6,38 @@ public class oneShotHiLo {
         Random random = new Random();
         int randomNumber = 1 + random.nextInt(100);
         int guess;
+        int attempts = 1;
+        System.out.println("I'm thinking of a number between 1-100. Try to guess it. You have 7 guesses.");
+        System.out.println("Attempt: #" + attempts);
+        guess = scan.nextInt();
+        while(guess != randomNumber && attempts <= 7)
+        {
+            if(guess > randomNumber)
+            {
+                System.out.println("Sorry, you are too high.");
+                attempts++;
+            }
+            if(guess < randomNumber)
+            {
+                System.out.println("Sorry, you are too low.");
+                attempts++;
+            } 
+            System.out.println("Attempt: #" + attempts);
+            guess = scan.nextInt();
+        }
+    }
+}
+
+//Here is the original code 
+/*
+import java.util.Random;
+import java.util.Scanner;
+public class oneShotHiLo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = 1 + random.nextInt(100);
+        int guess;
         System.out.println("I'm thinking of a number between 1-100. Try to guess it.");
         guess = scan.nextInt();
         if(guess == randomNumber)
@@ -23,3 +55,4 @@ public class oneShotHiLo {
         }
     }
 }
+*/
