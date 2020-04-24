5c5f2acc0dc88186c71e1322d922727edf8ef057
clocknight <EricFoote2014@hotmail.com>
2020-04-24 12:10:18 -0300

    Weirdbug if I used a do while loop instead of just a while loop so I am just going to do the while loop instead

--- PraticeProblems/safeSquareRoot/safeSquareRoot.java
+++ PraticeProblems/safeSquareRoot/safeSquareRoot.java
@@ -1,7 +1,19 @@
-import java.util.Scanner;   
+import java.util.Scanner;
+import java.lang.Math;   
 public class safeSquareRoot {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        double square;
+        // Math has no constructor so we dont need to do anything similar to scanner or random as we have seen this far
+        System.out.println("SQUARE ROOT!");
+        System.out.print("Enter a number:");
+        square = scan.nextDouble();
+        while(square < 0)
+        {
+            System.out.println("We are only dealing with real roots so no negative values");
+            System.out.print("Try again:");
+            square = scan.nextDouble();
+        } 
+        System.out.println("The square root of " + square + " is " + Math.sqrt(square));
     }
 }
