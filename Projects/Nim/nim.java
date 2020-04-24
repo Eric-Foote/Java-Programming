d8876b2d43070554e27bafe205197f7885770d20
clocknight <EricFoote2014@hotmail.com>
2020-04-24 15:45:48 -0300

    Decided instead to throw all of the introductory stuff into a constructor and just use main to call the constructor

--- Projects/Nim/nim.java
+++ Projects/Nim/nim.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;   
 public class nim {
+
+    public nim (){
+        Scanner scan = new Scanner(System.in);
+        Random r = new Random();
+        int a = 1 + r.nextInt(5); // we are first going to get the logic down when we have piles of size 5 
+        int b = 1 + r.nextInt(5);
+        int c = 1 + r.nextInt(5);
+        int firstPlayer = 1 + r.nextInt(2);
+        System.out.print("Player 1: enter your name: ");
+        String name1 = scan.next();
+        System.out.print("Player 2: enter your name: ");
+        String name2 = scan.next();
+        if(firstPlayer == 1)
+        {
+            System.out.println(name1 + " is Player 1");
+            //player1 = name1;
+            //player2 = name2;
+            game(a, b, c, name1, name2);
+        }
+        else
+        {
+            System.out.println(name2 + " is Player 1");
+            //player1 = name2;
+            // player2 = name1;   
+            game(a, b, c, name2, name1);   
+        }
+    }
     
     public static void game(int a, int b, int c, String player1, String player2) 
     {
@@ -26,31 +53,6 @@ public class nim {
     }
     
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random r = new Random();
-        int a = 1 + r.nextInt(5); // we are first going to get the logic down when we have piles of size 5 
-        int b = 1 + r.nextInt(5);
-        int c = 1 + r.nextInt(5);
-        int firstPlayer = 1 + r.nextInt(2);
-        System.out.print("Player 1: enter your name: ");
-        String name1 = scan.next();
-        System.out.print("Player 2: enter your name: ");
-        String name2 = scan.next();
-        if(firstPlayer == 1)
-        {
-            System.out.println(name1 + " is Player 1");
-            //player1 = name1;
-            //player2 = name2;
-            game(a, b, c, name1, name2);
-        }
-        else
-        {
-            System.out.println(name2 + " is Player 1");
-            //player1 = name2;
-            // player2 = name1;   
-            game(a, b, c, name2, name1);   
-        }
-        
-        
+        nim();
     }
 }
