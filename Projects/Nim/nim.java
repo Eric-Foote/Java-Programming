37d96f143846763c2fd43443cc744aed29bbe21a
clocknight <EricFoote2014@hotmail.com>
2020-04-24 15:43:04 -0300

    Decided to modify it so that there is a randomness for who gets player 1 and who gets player 2. Decided instead of having one giant main function to put the game logicin its own function called game

--- Projects/Nim/nim.java
+++ Projects/Nim/nim.java
@@ -1,6 +1,56 @@
-import java.util.Scanner;   
+import java.util.Scanner;
+import java.util.Random;   
 public class nim {
+    
+    public static void game(int a, int b, int c, String player1, String player2) 
+    {
+        int turnNumber = 1;
+        int amountToRemove = 0;
+        System.out.println("A: " + a + "\tB: " + b + "\tC: " + c);
+        String move;
+        while(a != 0 && b != 0 && c != 0)
+        {
+            if(turnNumber % 2 == 0)
+            {
+                System.out.println(player2 + "choose a pile: ");
+                move = scan.next();
+                turnNumber++;
+            }
+            else
+            {
+                System.out.println(player1 + "choose a pile: ");
+                move = scan.next();
+                turnNumber++; 
+            }
+        }
+    }
+    
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random r = new Random();
+        int a = 1 + r.nextInt(5); // we are first going to get the logic down when we have piles of size 5 
+        int b = 1 + r.nextInt(5);
+        int c = 1 + r.nextInt(5);
+        int firstPlayer = 1 + r.nextInt(2);
+        System.out.print("Player 1: enter your name: ");
+        String name1 = scan.next();
+        System.out.print("Player 2: enter your name: ");
+        String name2 = scan.next();
+        if(firstPlayer == 1)
+        {
+            System.out.println(name1 + " is Player 1");
+            //player1 = name1;
+            //player2 = name2;
+            game(a, b, c, name1, name2);
+        }
+        else
+        {
+            System.out.println(name2 + " is Player 1");
+            //player1 = name2;
+            // player2 = name1;   
+            game(a, b, c, name2, name1);   
+        }
+        
         
     }
 }
