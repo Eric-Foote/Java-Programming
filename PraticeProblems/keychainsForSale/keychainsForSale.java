88df1331e1d03a99772c81d464211696026fde83
clocknight <EricFoote2014@hotmail.com>
2020-04-25 11:38:42 -0300

    Didthis pratice problem all we had to do was get the output and function calls correct

---
+++ PraticeProblems/keychainsForSale/keychainsForSale.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class keychainsForSale {
+    public static void add_keychains()
+    {
+        System.out.println("ADD KEYCHAINS");
+    }
+    public static void remove_keychains()
+    {
+        System.out.println("REMOVE KEYCHAINS");
+    }
+    public static void view_order()
+    {
+        System.out.println("VIEW ORDER");
+    }
+    public static void checkout()
+    {
+        System.out.println("CHECKOUT");
+    }
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int input;
+        System.out.println("Ye Olde Keychain Shoppe");
+        System.out.println("1. Add Keychains to Order");
+        System.out.println("2. Remove Keychains to Order");
+        System.out.println("3. View Current Order");
+        System.out.println("4. Checkout");
+        System.out.println("");
+        System.out.println("Please enter your choice: ");
+        input = scan.nextInt();
+        System.out.println("");
+        while(input != 4)
+        {
+            if(input == 1)
+            {
+                add_keychains();
+            }
+            else if(input == 2)
+            {
+                remove_keychains();
+            }
+            else if(input == 3)
+            {
+                view_order();
+            }
+            System.out.println("");
+            System.out.println("Ye Olde Keychain Shoppe");
+            System.out.println("1. Add Keychains to Order");
+            System.out.println("2. Remove Keychains to Order");
+            System.out.println("3. View Current Order");
+            System.out.println("4. Checkout");
+            System.out.println("");
+            System.out.println("Please enter your choice: ");
+            input = scan.nextInt();
+            System.out.println("");
+
+        }
+        if(input == 4)
+        {
+            checkout();
+        }
+    }
+}
