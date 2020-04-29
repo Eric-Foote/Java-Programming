11be7f4e917a989808f672731aa9847eac3be9e9
clocknight <EricFoote2014@hotmail.com>
2020-04-29 16:01:45 -0300

    Wrote basic functions for the calculator

--- Projects/Calculator/calculator.java
+++ Projects/Calculator/calculator.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class calculator {
-    public static void main(String[] args) {
+    public static double addition(double operand1, double operand2)
+    {
+        return operand1 + operand2;
+    }
+    public static double subtraction(double operand1, double operand2)
+    {
+        return operand1 - operand2;
+    }
+    public static double multiplication(double operand1, double operand2)
+    {
+        return operand1 * operand2;
+    }
+    public static double division(double operand1, double operand2)
+    {
+        if(operand2 == 0)
+        {
+            System.out.println("Can't divide by zero");
+            return 0;
+        }
+        return operand1 / operand2;
+    }
+    public static double exponents(double operand1, double operand2)
+    {
+        return Math.pow(operand1, operand2);
+    }
+    public static void main(String[] args) 
+    {
         Scanner scan = new Scanner(System.in);
         //https://stackoverflow.com/questions/12524243/how-do-i-make-java-register-a-string-input-with-spaces source for the fix
         String statement = scan.nextLine(); // this will read the whole line of input rather then the first character only
@@ -8,8 +35,25 @@ public class calculator {
         double operand1 = Double.valueOf(Character.toString(statement.charAt(0))); // cast the first piece of the input string as a double
         String operation =  Character.toString(statement.charAt(1)); // cast the operand as a string 
         double operand2 = Double.valueOf(Character.toString(statement.charAt(2)));; // cast the third piece of input string as a double
-        System.out.println(operand1);
-        System.out.println(operation);
-        System.out.println(operand2);
+        if(operation.equals("+"))
+        {
+            System.out.println(addition(operand1, operand2));
+        }
+        else if(operation.equals("-"))
+        {
+            System.out.println(subtraction(operand1, operand2));
+        }
+        else if(operation.equals("*"))
+        {
+            System.out.println(multiplication(operand1, operand2));
+        }
+        else if(operation.equals("/"))
+        {
+            System.out.println(division(operand1, operand2));   
+        }
+        else if(operation.equals("^"))
+        {
+            System.out.println(exponents(operand1, operand2));
+        }
     }
 }
