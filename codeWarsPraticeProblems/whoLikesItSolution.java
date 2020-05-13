c06b1315c4bca71d9a411f93361c4a47db15bd78
clocknight <EricFoote2014@hotmail.com>
2020-05-13 12:50:20 -0300

    Finished this pratice problem

--- codeWarsPraticeProblems/whoLikesItSolution.java
+++ codeWarsPraticeProblems/whoLikesItSolution.java
@@ -14,10 +14,38 @@
 */
 public class whoLikesItSolution {
     public static String whoLikesIt(String[] names) {
-        //Do your magic here
-        return "";
+        // this is a correct solution that was tested on the site the only things I needed to change was the spacing and formatting the output
+        if (names.length == 0)
+        {
+            return "no one likes this";
+        }
+        else if (names.length == 1) 
+        {
+            return names[0] + " likes this";
+        }
+        else if (names.length == 2)
+        {
+            return names[0] + " and " + names[1] + " like this";
+        }
+        else if (names.length == 3)
+        {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        }
+        else
+        {
+            return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others" + " like this";
+        }
     }
     public static void main(String[] args) {
-        
+        String [] names0 = {};
+        String [] names1 = {"Fred"};
+        String [] names2 = {"Fred", "Bob"};
+        String [] names3 = {"Fred", "Bob", "Joe"};
+        String [] names4 = {"Fred", "Bob", "Joe", "Dog"};
+        System.out.println(whoLikesIt(names0));
+        System.out.println(whoLikesIt(names1));
+        System.out.println(whoLikesIt(names2));
+        System.out.println(whoLikesIt(names3));
+        System.out.println(whoLikesIt(names4));
     }
 }
