d26c67c45f2188e34b60a675e27d380859a19ec1
clocknight <EricFoote2014@hotmail.com>
2020-06-19 15:59:24 -0300

    going to work on this program and just do something basic with it

--- Projects/Blackjack/blackjack.java
+++ Projects/Blackjack/blackjack.java
@@ -1,4 +1,14 @@
-import java.util.Scanner;   
+/* Write a program that allows a human user to play a simplified version of Blackjack against a computer opponent.
+ * The simplified blackjack rules are as follows:
+ * Don't worry about suits and face cards; "cards" will have values from 2-11, and all values are equally likely 
+ * Draw two cards for the player and display them.
+ * Draw two cards for the dealer and display one of them, keeping the other one hidden.
+ * Allow the player to "hit" as many times as they would like.
+ * If the player "busts" (gets a total over 21), the dealer automatically wins.
+ * Allow the dealer to hit as many times as they would like. Dealer should probably hit on sixteen or lower.
+ * If the dealer busts, the player automatically wins
+ * Assuming no one has busted the player with the highest total wins
+ */
 import java.util.Random;
 public class blackjack {
     public static void main(String[] args) {
