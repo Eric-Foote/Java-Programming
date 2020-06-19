74696b89438719d127b72e5548fdae2b2f51871d
Eric Foote <Eric@localhost.localdomain>
2020-06-19 20:18:39 -0300

    Testing pulling and pushing across two computers

--- PraticeProblems/HelloWorld/helloworld.java
+++ PraticeProblems/HelloWorld/helloworld.java
@@ -3,5 +3,6 @@ public class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello World\n");
         System.out.println("Testing to make sure that codium is set up properly");
+	System.out.println("Testing this on a laptop to make sure javac and vim are working properly also to make sure that I can pull changes across two computers");
     }
 }
