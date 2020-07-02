4c6c9b094ae13df13fc2ec78471113486a463a61
clocknight <EricFoote2014@hotmail.com>
2020-07-02 15:42:08 -0300

    Re-did the first basic arrays problem this time using loops and indexes instead of manually inserting values

---
+++ PraticeProblems/basicArrays/basicArrays1.java
@@ -0,0 +1,9 @@
+public class basicArrays1 {
+	public static void main (String[] args) {
+		int [] arr = new int[10];
+		for(int i=0; i< arr.length; i++){
+			arr[i] = -113;
+			System.out.println("Slot " + i + " contains a " + arr[i]);
+		}
+	}
+}
