7fde7acb7bd72548be61e6f4012e562bb548a158
clocknight <EricFoote2014@hotmail.com>
2020-07-08 15:15:52 -0300

    Started this pratice problem which is a combination of two programming problems from the textbook

---
+++ praticeProblems2/chapter7Object-Oriented-Design/School.java
@@ -0,0 +1,20 @@
+/* Write a class called Course that represents a course taken at a school. Represent each student using
+ * the modified Student class from the previous programming project. Use an ArrayList in the Course
+ * class should accept only the name of the course. Provide a method called addStudent that accepts one
+ * Student parameter. Provide a method called average that computes and returns the average of all
+ * students' test score averages. Provide a method called roll that prints all the students in the
+ * course. Create a driver class with a main method that creates a course, adds several students,
+ * prints a roll, and prints the overall course test average. Modify the Student class presented
+ * in the chapter as follows, Each student object should also contain the scores for three tests.
+ * Provide a constructor that sets all instance values based on paramter values. Overload the 
+ * constructor such that each test score is assumed to be initally zero. Provide a method called
+ * setTestScore that accepts two parameters: the test number (1 through 3) and the score. Also 
+ * provide a method called getTestScore that accepts the test number and returns the appropriate 
+ * score. Provide a method called average that computes and returns the average test score for this
+ * student. Modify the toString method such that teh test scores and average are included in the 
+ * description of the student. Modify the driver class main method to exerciee the new Student
+ * methods.
+ * We are given some code to start with on pg.318
+*/
+
+import java.util.ArrayList;
