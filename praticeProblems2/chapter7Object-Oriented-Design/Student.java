85d8d534dc4a3daf98b3d915671cfe755b32a8d0
clocknight <EricFoote2014@hotmail.com>
2020-07-09 12:19:18 -0300

    Finished the Student file

--- praticeProblems2/chapter7Object-Oriented-Design/Student.java
+++ praticeProblems2/chapter7Object-Oriented-Design/Student.java
@@ -25,14 +25,42 @@ public class Student {
 		testScore3 = 0;
 	}
 
-	public setTestScore( int testScore, int testNumber)
+	public setTestScore( int testScore, int tN)
 	{
+		testNumber = tN;
+		if(tN == 1) {
+			testScore1 = testScore;
+		}
+		if(tN == 2) {
+			testScore2 = testScore;
+		}
+		if(tN == 3) {
+			testScore3 = testScore;
+		}	
+	}
 	
+	public int getTestScore(int tN) {
+		if(tN == 1)
+		{
+		 	return testScore1;
+		}
+		if(tN == 2)
+		{
+			return testScore2;
+		}
+		if(tN == 3)
+		{
+			return testScore3;
+		}
+
+	public double average(){
+		double avg = (testScore1 + testScore2 + testScore3)/3;
+		return avg;
 	}
 
         public String toString()
         {
-                String result = firstName + " " + lastName + " " + "\n" + "Home Address:\n" + homeAddress + "\n" + "School Address:\n" + schoolAddress;
+                String result = firstName + " " + lastName + " " + "\n" + "Home Address:\n" + homeAddress + "\n" + "School Address:\n" + schoolAddress + "\n" + "Test Score 1:" + testScore1 + "Test Score 2:" + testScore2 + "Test Score 3:" + testScore3 + "\n" + "Average: " + average();
                 return result;
         }
 }
